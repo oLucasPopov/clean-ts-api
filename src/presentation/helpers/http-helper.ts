89ee8be0314fca412459ea66f70b20dafa81f320
Lucas Popov <lucaspopov@protonmail.com>
2021-12-25 20:51:56 -0300

    refactor: change import

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
 import { httpResponse } from '../protocols/http'
-import { ServerError } from '../errors/server-error'
+import { ServerError } from '../errors'
 
 export const badRequest = (error: Error): httpResponse => ({
   statusCode: 400,
