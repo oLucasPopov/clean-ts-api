d71fcef0511a8ec3d56260003af53d2429c18b23
Lucas Popov <lucaspopov@protonmail.com>
2022-01-20 21:54:09 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -16,15 +16,17 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Lucas',
-        email: 'lucaspopov@protonmail.com',
-        password: 'senha123',
-        passwordConfirmation: 'senha123'
-      })
-      .expect(200)
+  describe('Post/SignUp', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Lucas',
+          email: 'lucaspopov@protonmail.com',
+          password: 'senha123',
+          passwordConfirmation: 'senha123'
+        })
+        .expect(200)
+    })
   })
 })
